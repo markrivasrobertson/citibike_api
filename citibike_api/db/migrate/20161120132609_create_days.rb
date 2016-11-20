020430cb1647fde7efc19a624888e54004bde53e
Mark Robertson <markrivasrobertson@yahoo.com>
2016-11-20 08:48:03 -0500

    change migration file to make the :date a t.string because our csv has the date in 1/12/2016 format

--- citibike_api/db/migrate/20161120132609_create_days.rb
+++ citibike_api/db/migrate/20161120132609_create_days.rb
@@ -1,7 +1,7 @@
 class CreateDays < ActiveRecord::Migration[5.0]
   def change
     create_table :days do |t|
-      t.integer :date
+      t.string :date
       t.integer :daily_rides
       t.integer :total_rides
 
