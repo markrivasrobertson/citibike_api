02d25edc7155d373a6bd9446414ae48b6b0623f3
Mark Robertson <markrivasrobertson@yahoo.com>
2016-11-20 09:57:33 -0500

    set up seeds.rb

--- citibike_api/db/seeds.rb
+++ citibike_api/db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "csv"
+
+CSV.foreach("../lib/assets/ride_data.csv", headers: true) do |row|
+  Day.create(date: row["date"], daily_rides: row["daily_rides"], total_rides: row["total_rides"])
+end
+  
