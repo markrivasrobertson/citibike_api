47a616555aa128492162a377c544c7a688a99aa7
Mark Robertson <markrivasrobertson@yahoo.com>
2016-12-09 14:24:59 -0500

    change the routes to include 'namespace :days' to allow api calls

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  resources :days
+  namespace :days do
+    resources :days
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
