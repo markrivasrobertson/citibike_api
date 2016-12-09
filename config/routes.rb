b91593007fea1040f9120be6e82cf493bc69e5bd
Mark Robertson <markrivasrobertson@yahoo.com>
2016-12-09 15:37:10 -0500

    reverse last committed changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  namespace :days do
-    resources :days
-  end
+  resources :days
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
